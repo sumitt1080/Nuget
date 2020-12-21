069862c09257a31e647896db6b651cf7b82ddc43
sumitt1080 <sumitt1080@gmail.com>
2020-12-21 22:22:20 +0530

    homedone

---
+++ android/app/src/main/kotlin/com/example/nuget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nuget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:connection_status_bar/connection_status_bar.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import './pages/home.dart';
+
+void main() async{
+   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+ @override
+  Widget build(BuildContext context) {
+    Align(
+      alignment: Alignment.topCenter,
+      child: ConnectionStatusBar(
+        title: Text('No Network Connectivity'),
+      ),
+    );
+    return MaterialApp(
+      title: 'Nuget',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        accentColor: Colors.teal,
+      ),
+      home: Home(),
+      
+    );
+    
+  }
+}
